a3ee816468e126661a4d73d7b75f58f8019287d1
Ephraim Kunz <ephraimkunz@me.com>
2020-06-10 17:53:27 -0700

    1 done

--- aoc01/src/main.rs
+++ aoc01/src/main.rs
@@ -1,3 +1,12 @@
+use std::fs::File;
+use std::io::{self, BufRead};
+
 fn main() {
-    println!("Hello, world!");
+    let file = File::open("src/input/input.txt").expect("Input file not found");
+    let reader = io::BufReader::new(file);
+    let freq = reader
+        .lines()
+        .map(|l| l.unwrap_or(String::from("0")).parse::<i32>().unwrap_or(0))
+        .fold(0, |accum, elem| accum + elem);
+    println!("{}", freq);
 }
